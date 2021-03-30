a0f10d5b281ed3a4308e1778d7d2cb14a0c1800a
Bryan <bry@DESKTOP-MGIN8VI>
2021-03-30 08:26:48 -0500

    Tarea1 -21

---
+++ Trabajo1-21/Trabajo1-21/Trabajo1-21.cpp
@@ -0,0 +1,20 @@
+// Trabajo1-21.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a;
+	int b;
+	cout << "Ingrese un primer valor: " << endl;
+	cin >> a;
+	cout << "Ingrese un segundo valor: " << endl;
+	cin >> b;
+	cout << "La suma es: " << a + b << endl;
+	cout << "La resta es: " << a - b << endl;
+	cout << "La multiplicacion es: " << a * b << endl;
+	cout << "La division es: " << a / b << endl;
+
+}
