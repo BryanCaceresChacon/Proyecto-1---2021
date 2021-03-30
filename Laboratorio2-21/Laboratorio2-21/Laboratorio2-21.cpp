5a8c6f38487ec96c27b4f78e4884ffecb9496db0
Bryan <bry@DESKTOP-MGIN8VI>
2021-03-30 08:32:23 -0500

    Laboratorio2

---
+++ Laboratorio2-21/Laboratorio2-21/Laboratorio2-21.cpp
@@ -0,0 +1,29 @@
+// Laboratorio2-21.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b, c, d;
+    cout << "Valor 1: ";
+    cin >> a;
+    cout << endl;
+    cout << "Valor 2: ";
+    cin >> b;
+    cout << endl;
+    cout << "Valor 3: ";
+    cin >> c;
+    cout << endl;
+    cout << "Valor 4: ";
+    cin >> d;
+    cout << endl;
+    cout << "La suma de valor 1 con valor 4 es: " << a + d << endl;
+    cout << "La multiplicacion de valor 3 con valor 2 es: " << c * b << endl;
+    cout << "La resta de valor 4 con valor 3 es: " << d - c << endl;
+    cout << "El resto de la division de valor 2 con valor 1 es: " << b % a << endl;
+    cout << "La division de valor 4 con valor 3 es: " << d / c << endl;
+
+}
+
